79d9b34b9c28b75e4271ebf9f74b72db785def41
Pedro Gomes <phbgomes@hotmail.com>
2021-10-08 08:51:37 -0300

    primeiro commit

---
+++ ContadorDia1/Tarefas.swift
@@ -0,0 +1,39 @@
+//
+//  Tarefas.swift
+//  ContadorDia1
+//
+//  Created by Pedro Gomes on 07/10/21.
+//
+
+import Foundation
+
+
+
+/*
+ 
+ Tarefas a serem feitas:
+ 
+    * Checar se cumpriu meta
+        - usar variável computada
+        - usar UIColor (218, 243, 255)
+            . digital color meter
+ 
+    * diferentes visualizações
+        - deixar o usuário ver o total em quantidade de copos, mls ou litros
+        - criar enum para a unidade
+        - criar struct para o consumo, tendo os valores e funcoes para conversão
+    
+    *  Meta não será dada pelo usuário, mas por nós
+        - struct para meta (ml, copos, dificuldade)
+        - enum dificuldade
+        - static var ml por copos
+        - trazer checar meta para essa struct
+        - criar vetor com várias metas diferentes
+ 
+    * Registro de água
+        - criar struct para o registro (mls e data)
+        - pegar data de agora
+        - apresentar na interface quando foi a última vez que bebemos agua
+ 
+ 
+ */
---
+++ ContadorDia1/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  ContadorDia1
+//
+//  Created by Pedro Gomes on 07/10/21.
+//
+
+
+/*
+ - Label => UILabel
+ - Botão => UIButton
+ - Imagem => UIImageView (view), e UIImage (própria imagem)
+ - Storyboard
+ 
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var labelQtdAgua: UILabel!
+    
+    @IBOutlet weak var botaoIncrementar: UIButton!
+    
+    @IBOutlet weak var botaoDiminuir: UIButton!
+    
+    @IBOutlet weak var imageViewCopoDagua: UIImageView!
+    
+    @IBOutlet weak var entradaDeTextoUsuario: UITextField!
+    
+    @IBOutlet weak var labelMeta: UILabel!
+    
+    //MARK: - Variáveis
+    
+    var contadorCoposDagua: Int = 0
+ 
+    // MARK: - Ciclo de Vida (Life Cycle)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let imagemCopoAgua: UIImage? = UIImage(named: "copoDeAgua")
+       
+        imageViewCopoDagua.image = imagemCopoAgua
+        
+        atualizaLabel()
+        
+        entradaDeTextoUsuario.addTarget(self, action: #selector(atualizaMeta), for: .editingChanged)
+
+    }
+
+    // MARK: - Metodos
+    func atualizaLabel() {
+        labelQtdAgua.text = "Copos hoje: " + String(contadorCoposDagua)
+    }
+    
+    @objc func atualizaMeta() {
+        // ??
+        // if let
+        // guard let
+        guard let textoDoUsuario = entradaDeTextoUsuario.text else {
+            return
+        }
+        labelMeta.text = "Meta: " + textoDoUsuario
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func incrementaContador(_ sender: UIButton) {
+        contadorCoposDagua += 1
+        atualizaLabel()
+    }
+    
+    @IBAction func diminuiContador(_ sender: UIButton) {
+        
+        if (contadorCoposDagua > 0) {
+            contadorCoposDagua -= 1
+            atualizaLabel()
+        }
+    }
+}
+
+
+
